fb6c25ec168e17ca2df9171933e69a99fdd06e59
Julian C. Dunn <jdunn@getchef.com>
2014-11-11 00:51:12 -0500

    Update to 2.0.0.20141103

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-default['iterm2']['version']  = "-1_0_0_20140421"
+default['iterm2']['version']  = "-2_0_0_20141103"
 default['iterm2']['beta_or_stable'] = "beta"
 default['iterm2']['checksum'] = "d9f3dd198febf46c272a8d4b473e35cae4cdfbf1963a7796114df53b5cb06c63"
 
